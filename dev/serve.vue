c992896732c1933b48cc5ce464219ab5d7cd5c6f
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-01-30 12:28:35 +0100

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from "vue";
+import VueSelectSfc from "~/vue-select-sfc.vue";
+
+export default Vue.extend({
+  name: "ServeDev",
+  components: {
+    VueSelectSfc,
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-select-sfc />
+  </div>
+</template>
