2a1a83bcd306c9dcb6e685930681931beaee6805
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-01-30 17:25:16 +0100

    Added support for jest

--- tests/helpers.js
+++ tests/helpers.js
@@ -4,7 +4,7 @@ import VueSelect from '../src/components/SelectSfc.vue'
 export const mountDefault = (props = {}, options = {}) => {
   return shallowMount(VueSelect, {
     propsData: {
-      options: ['one', 'two', 'three'],
+      options: ['option 1', 'option 2', 'option 3'],
       ...props,
     },
     ...options,
